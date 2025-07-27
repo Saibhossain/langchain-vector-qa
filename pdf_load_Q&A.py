ad105d20cbd3f6d4c1338f034387f9bf544c0e0f
mdsaibhossain <saibhossain5@gmail.com>
2025-07-28 02:40:18 +0600

    chat bot working

--- pdf_load_Q&A.py
+++ pdf_load_Q&A.py
@@ -2,8 +2,9 @@ from langchain_community.document_loaders import PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
-from langchain.chains import RetrievalQA
-from langchain_community.llms import Ollama  # Use this if running locally
+from langchain.chains.retrieval_qa.base import RetrievalQA
+from langchain.chains import ConversationalRetrievalChain
+from langchain_ollama import OllamaLLM # Use this if running locally
 from sqlalchemy.dialects.mysql.mariadb import loader
 
 
@@ -20,14 +21,29 @@ print(f"✅ Split into {len(chunks)} chunks.")
 embedding_model = HuggingFaceEmbeddings(model_name='sentence-transformers/all-MiniLM-L6-v2')
 
 vectorstore = FAISS.from_documents(chunks,embedding_model)
-retriever = vectorstore.as_retriever(search_type="similarity", search_kwargs={"k": 3})
+
 print("✅ FAISS vectorstore created with embedded chunks.")
 
-llm = Ollama(model="gemma3n:latest")
+llm = OllamaLLM(model="gemma3n:latest")
 
 qa_chain = RetrievalQA.from_chain_type(
     llm=llm,
-    retrieve=retriever,
+    retriever=vectorstore.as_retriever(),
     chain_type="stuff",
     return_source_documents=True
 )
+
+while True:
+    query = input("📘 Ask a question (or type 'exit'): ")
+    if query.lower() == "exit":
+        break
+
+    result = qa_chain({"query": query})
+    print("\n🧠 Answer:")
+    print(result["result"])
+
+    # Optional: Show source pages
+    print("\n📄 Source Snippet:")
+    for doc in result["source_documents"][:1]:
+        print(doc.page_content[:300])
+        print("-" * 50)
